50ca70a27874807971f2ecdc39756d77a3c5d7f8
Shane Louis Barita <89013136+baristabarita@users.noreply.github.com>
2024-08-26 00:04:01 +0800

    [FEAT] added a postbanner text and main background image

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,27 @@
-import { useState } from 'react'
-import { Button } from "@/components/ui/button"
-import Navbar from './components/navbar/navbar'
-import Banner from './components/banner/banner'
+import { useState } from "react";
+import Navbar from "./components/navbar/navbar";
+import Banner from "./components/banner/banner";
+import PostBanner from "./components/postbanner/postbanner";
 
+import BackgroundImage from "./assets/images/backgroundimage.png";
+
+import { Button } from "@/components/ui/button";
 function App() {
-
   return (
     <>
       <Navbar />
-      <div className='w-full animate-fade-in overflow-x-hidden'>
+      <div className="w-full animate-fade-in overflow-x-hidden">
         <Banner />
-        <Button>Click me</Button>
+        <div
+          style={{
+            backgroundImage: `url(${BackgroundImage})`,
+          }}
+        >
+          <PostBanner />
+        </div>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
