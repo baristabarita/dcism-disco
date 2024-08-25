752036da4020c9ab87d89e3a6fa8b93b23720669
Shane Louis Barita <89013136+baristabarita@users.noreply.github.com>
2024-08-25 20:19:32 +0800

    [FEAT] Added Shadcn UI for reusable components

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import { Button } from "@/components/ui/button"
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -25,6 +26,9 @@ function App() {
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
+      <div>
+      <Button>Click me</Button>
+    </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
